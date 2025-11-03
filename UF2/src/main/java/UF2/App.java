4f23eddaf01b3e54b805705af44b19ed2fd2322e
adriansr22 <asanchezr@inscastellet.cat>
2025-11-03 19:13:07 +0100

    Primer ex uf2

--- UF2/src/main/java/UF2/App.java
+++ UF2/src/main/java/UF2/App.java
@@ -1,4 +1,4 @@
-package Clase.Ex;
+package querys;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
 
-public class MostrarSports {
+public final class App {
 
     public static void main(String[] args) {
 
